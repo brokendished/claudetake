ec6f1c66e1e02e1a395e04276b633137869364cd
brokendished <brokendished@gmail.com>
2025-04-17 17:46:50 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
+import FirebaseAuthSync from '../components/FirebaseAuthSync';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <FirebaseAuthSync />
       <Component {...pageProps} />
     </SessionProvider>
   );
