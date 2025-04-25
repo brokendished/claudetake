1fdee599fc1743fe90b5d0f78c805218aa9267b0
brokendished <brokendished@gmail.com>
2025-04-25 14:18:07 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
-import '@/styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
-import FirebaseAuthSync from '../components/FirebaseAuthSync';
+i// pages/_app.js
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <SessionProvider session={session}>
-      <FirebaseAuthSync />
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+import '@/styles/globals.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../libs/firebaseClient';
+
+export default function MyApp({ Component, pageProps }) {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return <div className="h-screen flex items-center justify-center">Loading…</div>;
+  }
+
+  // Optionally, you can redirect to /login here if you want all pages protected:
+  // if (!user) { 
+  //   if (typeof window !== 'undefined') window.location.href = '/login';
+  //   return null;
+  // }
+
+  return <Component {...pageProps} user={user} />;
 }
